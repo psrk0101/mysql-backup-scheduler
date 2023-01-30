576453480370e507f937dd15699078f5b3348963
sangjun.park <psrk0101@gmail.com>
2023-01-30 11:19:55 +0900

    Final Test End

--- src/scheduler.js
+++ src/scheduler.js
@@ -7,22 +7,36 @@ import { dumpLocation } from './config/config.js';
 import { removeFolder } from './fileService.js';
 
 const pathFormat = (date) => {
-	let dateFormat2 = date.getFullYear() +
-		'/' + ( (date.getMonth()+1) < 9 ? "0" + (date.getMonth()+1) : (date.getMonth()+1) )+
-		'/' + ( (date.getDate()) < 9 ? "0" + (date.getDate()) : (date.getDate()) );
-        // '/' + ( (date.getMinutes()) < 9 ? "0" + (date.getMinutes()) : (date.getMinutes()) );
+	let dateFormat2 = date.getFullYear()
+		+ '/' + ( (date.getMonth()+1) < 9 ? "0" + (date.getMonth()+1) : (date.getMonth()+1) )
+		+ '/' + ( (date.getDate()) < 9 ? "0" + (date.getDate()) : (date.getDate()) )
+        // + '/' + ( (date.getHours()) < 9 ? "0" + (date.getHours()) : (date.getHours()) )
+        ;
+        
 	return dateFormat2;
 }
 
 export async function run(){
-    cron.schedule(schedule, () => {
-        const date = new Date();
+    const date = new Date();
         let path = dumpLocation + pathFormat(date) + '/';
+        console.log(path);
         getDatabases().then(async databases => {
             const files = await dump(path, databases);
+            console.log(files)
             const promises = files.map(file => uploadFile(file));
             await Promise.all(promises);
             removeFolder(dumpLocation);
         })
-    })
+    // cron.schedule(schedule, () => {
+    //     const date = new Date();
+    //     let path = dumpLocation + pathFormat(date) + '/';
+    //     console.log(path);
+    //     getDatabases().then(async databases => {
+    //         const files = await dump(path, databases);
+    //         console.log(files)
+    //         const promises = files.map(file => uploadFile(file));
+    //         await Promise.all(promises);
+    //         removeFolder(dumpLocation);
+    //     })
+    // })
 }
